299a21ffea296c019927609df909ba9b538d6e0b
makrau <iago006@hotmail.com>
2022-11-02 13:49:13 -0300

    configuracoes iniciais

---
+++ prisma/migrations/20221102011409_create_pool_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Pool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pool_code_key" ON "Pool"("code");
